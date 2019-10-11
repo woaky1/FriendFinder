696000286159fd26a38afbf364af233706fa580e
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-10 19:33:36 -0500

    able to get JSON info by typing in api address.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,16 @@
 var friends = require("../data/friends");
+
+module.exports = function (server) {
+    server.post("/api/userdata", function (req, res) {
+        console.log(req.body);
+        userInfo.push(req.body);
+        console.log("yes post is working");
+        res.json(req.body)
+
+    });
+
+    server.get("/api/friends", function (req, res) {
+        res.json(friends);
+    })
+
+};
