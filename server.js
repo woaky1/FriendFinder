52c863e0a914b0cbff38871958b249c38fb2f30e
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-12 21:17:54 -0500

    rolled back changes to the last good commit.

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ var PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+<<<<<<< HEAD
 app.use(express.static( __dirname + '/public' ));
 app.use('/static', express.static(__dirname + '/public'));
 
+=======
+>>>>>>> parent of e642a0b... I got the ajax javascript into it's own file by adding code to server.js pointing where static files come from.
 
 require("./app/routing/apiRoutes")(app)
 require("./app/routing/htmlRoutes")(app)
