5aedd58e24e20bc9241eba6b0eba2367cbe22f17
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-12 21:14:11 -0500

    Gonna roll back these changes and go back to coding in script tags.

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use( express.static( __dirname + '/public' ));
+app.use(express.static( __dirname + '/public' ));
+app.use('/static', express.static(__dirname + '/public'));
+
 
 require("./app/routing/apiRoutes")(app)
 require("./app/routing/htmlRoutes")(app)
