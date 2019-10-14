418dbadda4a050c37dd2b80b0bf1eef6c7adc8c8
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-14 00:06:15 -0500

    added some server side validation just in case.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,14 +3,17 @@ var scoreCompare = require("../data/friendshipCalc");
 var score = [];
 module.exports = function (server) {
     server.post("/api/friends/:name/:photo/:q1/:q2/:q3/:q4/:q5/:q6/:q7/:q8/:q9/:q10", function (req, res) {
-        console.log(req.params);
-        var userInput = new FriendInfo(req.params);
-        friends.push(userInput);
-        console.log("yes post is working");
-        var response = scoreCompare(friends)
-        console.log(response);
-        return res.json(response);
-
+        if ((req.params.name && req.params.photo && req.params.q1 && req.params.q2 && req.params.q3 && req.params.q4 && req.params.q5 && req.params.q6 && req.params.q7 && req.params.q8 && req.params.q9 && req.params.q10) !== "null") {
+            console.log(req.params);
+            var userInput = new FriendInfo(req.params);
+            friends.push(userInput);
+            console.log("yes post is working");
+            var response = scoreCompare(friends)
+            console.log(response);
+            return res.json(response);
+        } else {
+            res.status(400).send('Bad Request');
+        }
     });
 
     server.get("/api/friends", function (req, res) {
