828d55746a37e5cface62df2620280d0a9fb3c7e
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-13 20:09:26 -0500

    Bad images no longerr display a broken link image. Working images do dispay, but probably need some size limits placed on them.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://cdn.bulbagarden.net/upload/5/54/Ash_SM.png",
         "scores":[
             5,
             1,
@@ -12,54 +12,6 @@ var friends = [
             2,
             5,
             4,
-            1
-        ]
-    },
-    {
-        "name" : 'Wario',
-        "photo" : 'Wa',
-        "scores" : [
-            1,
-            1,
-            2,
-            5,
-            3,
-            3,
-            3,
-            3,
-            4,
-            4
-        ]
-    },
-    {
-        "name": "Alex",
-        "photo": "Alex",
-        "scores": [
-            4,
-            3,
-            1,
-            5,
-            3,
-            5,
-            3,
-            5,
-            3,
-            3
-        ]
-    },
-    {
-        "name": "Paul",
-        "photo": "Paul",
-        "scores": [
-            2,
-            4,
-            4,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
             1
         ]
     }
