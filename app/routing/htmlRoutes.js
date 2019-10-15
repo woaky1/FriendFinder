9bf0c3496bca5054cd9a82b280dda48b1cc420a8
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-14 19:50:00 -0500

    Added inline comments for apiRoutes and htmlRoutes.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,14 @@
+// This javascript file handles requests related to html calls.
 var path = require("path");
 
 module.exports = function (app) {
+    // This serves the survey page when the url ends with '/survey'.
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"))
 
     });
 
+    //For all other, non-api request, we default to the home page.
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
